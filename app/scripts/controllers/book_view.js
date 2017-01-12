6d65eb663981a32683634d4451857bc4735a291a
sergey <qwq-qwq@yandex.ru>
2017-01-12 03:18:18 +0200

    filter

--- app/scripts/controllers/book_view.js
+++ app/scripts/controllers/book_view.js
@@ -8,26 +8,23 @@ angular.module('angularApp')
     var code = $location.search().code;
     $http.get(config.url() + '/api/books/search?code=' + code)
       .success(function (response) {
-        var images = new Array();
-        var book = response.booksList[0];
-        book.images = images;
+        $scope.book = response.booksList[0];
         var imagesCount = book.imagesCount;
-        if (book.image === '') {
-          book.image = '/img/no_picture_ru_165.jpg';
+        if ($scope.book.image === '') {
+          $scope.book.image = '/img/no_picture_ru_165.jpg';
         } else {
-          book.image = '/img/pics/' + book.code + '_big.jpg';
+          $scope.book.image = '/img/pics/' + book.code + '_big.jpg';
           imagesCount = imagesCount - 1;
         };
-        if (book.image_3d !== '') {
-          book.image_ = '/img/pics/' + book.code + '_0_big.jpg';
+        $scope.book.images = [];
+        if ($scope.book.image_3d !== '') {
           imagesCount = imagesCount - 1;
-          response[0].images[0] = {
+          $scope.book.images[0] = {
            'url': '/img/pics/' + code + '_0.jpg',
            'thumbUrl': '/img/pics/' + code + '_0.jpg',
            'thmb_index': 0
            };
         };
-        $scope.book = book;
         for (var i = 1; i <= imagesCount; i++) {
           $scope.book.images[i] = {
             'url': '/img/pics/' + code + '_' + i + '.jpg',
