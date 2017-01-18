fd1ec8448767b706db7f9064f8d218a06b54fc31
sergey <qwq-qwq@yandex.ru>
2017-01-18 20:38:50 +0200

    filter

--- app/scripts/controllers/book_view.js
+++ app/scripts/controllers/book_view.js
@@ -76,14 +76,14 @@ angular.module('angularApp')
     $http.get(config.url() + '/api/books/images?code=' + code)
       .success(function (response) {
         angular.forEach(response, function (image, key) {
-          if (!image.main) {
+
             $scope.gallery.images[key] = {
               srcThumbNail: image.src.replace('.jpg', '_big.jpg'),
               src: image.src,
               w: image.width,
               h: image.height
             };
-          }
+
         })
       })
 
