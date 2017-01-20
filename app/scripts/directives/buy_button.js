1fdc5eb8babfc893642e148953968dea4f9972b2
sergey <qwq-qwq@yandex.ru>
2017-01-21 00:45:55 +0200

    filter

--- app/scripts/directives/buy_button.js
+++ app/scripts/directives/buy_button.js
@@ -4,33 +4,41 @@ angular.module('angularApp').directive('bkBuyButton', ['$http', 'config', 'autho
   return {
     restrict: 'E',
     scope: {
-      book: '=',
-      cart: '='
+      book: '='
     },
     templateUrl: 'views/bk_buy_button.html',
     link: function(scope, element, attributes) {
-      scope.boughtText = "Купити";
-      scope.boughtDisable = false;
-      scope.AddToCart = function(book) {
-        if (authorization.isAuthorized()) {
-          if ($rootScope.cart === undefined){
-            $rootScope.cart = {email: authorization.username(), goodsTable: []}
+      scope.$watch('book', function () {
+        if (scope.book !== undefined) {
+          if ([1227, 2008].indexOf(scope.book.state) === -1) {
+            if (scope.book.kvo > 0) {
+              scope.boughtText = "Купити";
+              scope.boughtDisable = false;
+            } else {
+              scope.boughtText = "Замовити";
+              scope.boughtDisable = false;
+            }
+          } else {
+            scope.boughtText = "Купити";
+            scope.boughtDisable = true;
           }
-          var goodsTable = {code: book.code, quantity: 1, price: book.price, name: book.name}
-          $rootScope.cart.goodsTable = goodsTable;
+        }
+      })
+      scope.AddToCart = function(book) {
+        if ($rootScope.cart === undefined){
+          $rootScope.cart = {email: authorization.username(), goodsTable: []}
+        }
+        var goodsTable = {code: book.code, quantity: 1, price: book.price, name: book.name}
+        $rootScope.cart.goodsTable.push(goodsTable);
+        if (authorization.isAuthorized()) {
           $http.post(config.url() + "/api/edit/carts/update", $rootScope.cart, {withCredentials: true})
             .success(function(response) {
                scope.boughtText = "У кошику";
-              scope.boughtDisable = true;
+               scope.boughtDisable = true;
                $rootScope.cart = response;
                scope.$emit('cart_was_added');
             })
         }else{
-          if ($rootScope.cart === undefined){
-            $rootScope.cart = {email: authorization.username(), goodsTable: []}
-          }
-          var goodsTable = {code: book.code, quantity: 1, price: book.price, name: book.name}
-          $rootScope.cart.goodsTable.push(goodsTable);
           $http.post(config.url() + "/api/carts/update", $rootScope.cart)
             .success(function(response) {
                scope.boughtText = "У кошику";
