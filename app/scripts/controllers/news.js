328163af67ee9d664d7747072fbeaf8b55ae8c85
sergey <qwq-qwq@yandex.ru>
2017-02-06 10:07:45 +0200

    filter

--- app/scripts/controllers/news.js
+++ app/scripts/controllers/news.js
@@ -2,7 +2,7 @@
  * Created by sergey on 06.02.17.
  */
 angular.module('angularApp')
-  .controller('NewsCtrl', function ($scope, $http, $location, Lightbox, config) {
+  .controller('NewsCtrl', function ($scope, $http, $location, Lightbox, config, $sce) {
     var id = $location.search().id;
 
     function compare_desc(a,b) {
@@ -17,6 +17,11 @@ angular.module('angularApp')
     $http.get(config.url() + '/api/news/get_news_by_id?id=' + id)
       .success(function (response) {
         $scope.news = response;
+        angular.forEach(response, function(value, key) {
+          if (value.videoLink !== undefined) {
+            value.sceLink = $sce.trustAsResourceUrl(value.videoLink);
+          }
+        });
       })
 
     $http.get(config.url() + "/api/news")
