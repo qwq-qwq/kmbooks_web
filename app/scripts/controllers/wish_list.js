a4059d812d51da51447a75a4466aaace923ebf17
sergey <qwq-qwq@yandex.ru>
2017-02-13 22:01:26 +0200

    filter

--- app/scripts/controllers/wish_list.js
+++ app/scripts/controllers/wish_list.js
@@ -5,13 +5,27 @@
 'use strict';
 
 angular.module('angularApp')
-  .controller('WishListCtrl', function($scope, $http, $location, $anchorScroll, $route, config, authorization, utils) {
+  .controller('WishListCtrl', function($scope, $http, $location, $anchorScroll, $route, config, wishList, utils, authorization) {
     $scope.username = authorization.username();
 
     $http.get(config.url() + "/api/user/wish_lists/get_by_username?username=" + authorization.username(), {withCredentials: true})
       .success(function(response) {
         $scope.wishList = response;
       })
+
+    $scope.removeFromList = function (item) {
+      $http.post(config.url() + "/api/user/wish_lists/delete", item, {withCredentials: true})
+        .success(function(response) {
+          if ($scope.wishList !== undefined){
+            angular.forEach($scope.wishList, function(value, key) {
+              if (value.id === item.id) {
+                $scope.wishList.splice(key, 1);
+                wishList.SetWishList($scope.wishList);
+              }
+            });
+          }
+        })
+    }
 
     $scope.toDateTime = function(ObjId) {
       return utils.toDateTime(ObjId);
