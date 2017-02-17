219de127abc56a76f7a84c42b5470d1388fd9af5
sergey <qwq-qwq@yandex.ru>
2017-02-17 16:03:03 +0200

    filter

---
+++ app/scripts/directives/book_detail_info.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sergey on 17.02.17.
+ */
+'use strict';
+
+angular.module('angularApp').directive('bkBookDetailInfo', [function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/bk_book_detail_info.html'
+  };
+}])
