12539ddf7433822cf919db9424a5df4836144962
sergey <qwq-qwq@yandex.ru>
2017-03-03 13:45:03 +0200

    filter

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -4,7 +4,7 @@ angular.module('angularApp').factory('config', function () {
 
     return {
         url: function() {
-        var url = 'https://api.kmbooks.com.ua'; //'http://api.kmbooks.com.ua'; 'http://127.0.0.1:8080'
+        var url = 'https://api.kmbooks.com.ua'; //'https://api.kmbooks.com.ua'; 'http://127.0.0.1:8080'
         return url;
       }
     }
