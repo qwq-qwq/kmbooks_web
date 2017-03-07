8b906ebacc62f3b145d679fd71bef6fdc6d7dbf2
sergey <qwq-qwq@yandex.ru>
2017-03-07 18:50:09 +0200

    filter

---
+++ app/scripts/directives/price.js
@@ -0,0 +1,11 @@
+/**
+ * Created by sergey on 07.03.17.
+ */
+'use strict';
+
+angular.module('angularApp').directive('bkPrice', [function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'views/bk_price.html'
+  };
+}])
