576cdc35e849b0888d327a040559b0b4731ed84f
sergey <qwq-qwq@yandex.ru>
2017-04-03 14:19:52 +0300

    filter

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -218,7 +218,7 @@ angular.module('angularApp')
         if (orderAmount > 450) {
           $scope.deliveryCost = 0;
         } else {
-          $scope.deliveryCost = 35;
+          $scope.deliveryCost = 40;
         }
       }else if($scope.selectedDelivery.id === '4') {
         if (orderAmount > 450) {
