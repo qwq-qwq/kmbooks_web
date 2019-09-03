5486f5b189b71227f04a2f669e180377baf70b4c
qwq-qwq@yandex.ru <Z7DvzyEuQx8iymaqdD>
2019-09-03 16:15:49 +0300

    add font sizes for froala editor

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -280,7 +280,8 @@ angular
      fontFamily: {
        "Calibri": 'Calibri'
      },
-     fontFamilySelection: true
+     fontFamilySelection: true,
+     fontSize: ['8', '9', '10', '11', '12', '14', '18', '20', '22', '24', '30', '36', '48', '60', '72', '96']
   });
 ;
 
