badf1c31cb35156a79c539668f9d4b443d10cb71
OutlanderKiev <52034947+OutlanderKiev@users.noreply.github.com>
2019-09-06 17:24:21 +0300

    Update price.js

--- app/scripts/directives/price.js
+++ app/scripts/directives/price.js
@@ -18,12 +18,12 @@ angular.module('angularApp').directive('bkPrice', ['authorization', '$rootScope'
          scope.lineThroughStyle = {'margin-left': 'auto', 'margin-right': 'auto', 'max-width': '110px'};
       }
       scope.updateAuthorization = function () {
-        var discount = 25;
+        var discount = 0;
         if (authorization.isAuthorized()) {
           if (authorization.getUser().valueOfPurchases <= 1000){
-            discount = 25;
+            discount = 3;
           }else{
-            discount = 25;
+            discount = 5;
           }
         }
         if (scope.book !== undefined){
