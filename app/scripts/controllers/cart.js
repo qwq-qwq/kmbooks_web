3e6a3afd9ad33792149f395980b94fbd07b2b96a
OutlanderKiev <52034947+OutlanderKiev@users.noreply.github.com>
2019-09-12 17:52:36 +0300

    Update cart.js

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -308,7 +308,7 @@ angular.module('angularApp')
           $scope.deliveryCost = 45;
         }
       }else if($scope.selectedDelivery.id === '4') {
-        if ($scope.orderAmountWithDiscount >= 100) {
+        if ($scope.orderAmountWithDiscount >= 200) {
           $scope.deliveryCost = 0;
         } else {
           $scope.deliveryCost = 30;
