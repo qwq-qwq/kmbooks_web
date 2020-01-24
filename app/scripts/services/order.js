4ed1d39f76e833a573785fefc9a2537c68edca6f
sergey <qwq-qwq@yandex.ru>
2020-01-24 07:15:13 +0200

    fix

--- app/scripts/services/order.js
+++ app/scripts/services/order.js
@@ -95,7 +95,7 @@ angular.module('angularApp').factory('order', function (authorization, cart, con
         if (order.orderAmountWithDiscount >= 1000) {
           order.deliveryCost = 0;
         } else {
-          order.deliveryCost = 60;     
+          order.deliveryCost = 60;
         }
       }else if (order.delivery.id === '3'){
         order.deliveryCost = 0;
@@ -103,13 +103,10 @@ angular.module('angularApp').factory('order', function (authorization, cart, con
         if (order.orderAmountWithDiscount >= 1000) {
           order.deliveryCost = 0;
         } else {
-          order.deliveryCost = 45;        
+          order.deliveryCost = 45;
         }
       }else if(order.delivery.id === '4') {
         order.deliveryCost = 0;
-        else {
-         order.deliveryCost = 30;       
-        }
       }
       order.totalAmount = order.orderAmountWithDiscount + order.deliveryCost;
     }
