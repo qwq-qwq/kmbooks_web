e2641d6fb74d682021594d8844a2ad8b6aa3ff58
sergey <qwq-qwq@yandex.ru>
2020-01-31 17:06:45 +0200

    add fields for new settings

--- app/scripts/controllers/settings_edit_table.js
+++ app/scripts/controllers/settings_edit_table.js
@@ -18,7 +18,8 @@ angular.module('angularApp')
     }
     $scope.doneEditing = function (item) {
       item.editing = false;
-      var event = {id: item.id, recommended: item.recommended, soonOnSale: item.soonOnSale, customTop: item.customTop};
+      var event = {id: item.id, recommended: item.recommended, soonOnSale: item.soonOnSale, customTop: item.customTop,
+        actionText: item.actionText, actionTextMobile: item.actionTextMobile, actionLastDay: item.actionLastDay};
       $http.post(config.url() + "/api/edit/settings_update", event, {withCredentials: true})
         .success(function(response) {
             if (event.id == 0) {
